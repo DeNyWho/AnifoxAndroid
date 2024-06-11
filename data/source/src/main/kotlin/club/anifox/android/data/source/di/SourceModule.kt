7f7f7af20202729acdf9421b428983c0a5d4b9ea
Ахунов Денис <60143913+DeNyWho@users.noreply.github.com>
2024-06-11 21:46:57 +0300

    Add di for usecase

--- data/source/src/main/kotlin/club/anifox/android/data/source/di/SourceModule.kt
+++ data/source/src/main/kotlin/club/anifox/android/data/source/di/SourceModule.kt
@@ -4,6 +4,9 @@ import club.anifox.android.data.network.service.AnimeService
 import club.anifox.android.data.source.repository.AnimeRepositoryImpl
 import club.anifox.android.domain.repository.AnimeRepository
 import club.anifox.android.domain.usecase.anime.GetAnimeDetailUseCase
+import club.anifox.android.domain.usecase.anime.GetAnimeRelatedUseCase
+import club.anifox.android.domain.usecase.anime.GetAnimeScreenshotUseCase
+import club.anifox.android.domain.usecase.anime.GetAnimeSimilarUseCase
 import club.anifox.android.domain.usecase.anime.GetAnimeUseCase
 import dagger.Module
 import dagger.Provides
@@ -32,5 +35,23 @@ object SourceModule {
     @Singleton
     fun provideGetAnimeDetailUseCase(animeRepository: AnimeRepository): GetAnimeDetailUseCase {
         return GetAnimeDetailUseCase(animeRepository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGetAnimeSimilarUseCase(animeRepository: AnimeRepository): GetAnimeSimilarUseCase {
+        return GetAnimeSimilarUseCase(animeRepository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGetAnimeRelatedUseCase(animeRepository: AnimeRepository): GetAnimeRelatedUseCase {
+        return GetAnimeRelatedUseCase(animeRepository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGetAnimeScreenshotUseCase(animeRepository: AnimeRepository): GetAnimeScreenshotUseCase {
+        return GetAnimeScreenshotUseCase(animeRepository)
     }
 }
