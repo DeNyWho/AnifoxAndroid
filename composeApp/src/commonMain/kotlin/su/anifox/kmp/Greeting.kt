6db829aa2800b4315b78ab89adc1295c93cd4744
DeNyWho <denis.akhunov00@mail.ru>
2025-12-03 22:33:04 +0300

    ✨  feat: A new beginning ✨

---
+++ composeApp/src/commonMain/kotlin/su/anifox/kmp/Greeting.kt
@@ -0,0 +1,9 @@
+package su.anifox.kmp
+
+class Greeting {
+    private val platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
---
+++ composeApp/src/commonMain/kotlin/su/anifox/kmp/Platform.kt
@@ -0,0 +1,7 @@
+package su.anifox.kmp
+
+interface Platform {
+    val name: String
+}
+
+expect fun getPlatform(): Platform
